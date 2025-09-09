610dc940285fc020a86be89910b762acc4cc0810
muhammed sahel <frickzzpv@gmail.com>
2025-09-09 07:26:01 +0300

    WIP : docker production deployment -  styling is not working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,42 +7,42 @@ import Layout from '@/components/layout';
 const inter = localFont({
   src: [
     {
-      path: '../../public/fonts/web/Inter-Regular.woff2',
+      path: './fonts/web/Inter-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/web/Inter-Medium.woff2',
+      path: './fonts/web/Inter-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/web/Inter-SemiBold.woff2',
+      path: './fonts/web/Inter-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/web/Inter-Bold.woff2',
+      path: './fonts/web/Inter-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/web/Inter-Italic.woff2',
+      path: './fonts/web/Inter-Italic.woff2',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '../../public/fonts/web/Inter-MediumItalic.woff2',
+      path: './fonts/web/Inter-MediumItalic.woff2',
       weight: '500',
       style: 'italic',
     },
     {
-      path: '../../public/fonts/web/Inter-SemiBoldItalic.woff2',
+      path: './fonts/web/Inter-SemiBoldItalic.woff2',
       weight: '600',
       style: 'italic',
     },
     {
-      path: '../../public/fonts/web/Inter-BoldItalic.woff2',
+      path: './fonts/web/Inter-BoldItalic.woff2',
       weight: '700',
       style: 'italic',
     },
